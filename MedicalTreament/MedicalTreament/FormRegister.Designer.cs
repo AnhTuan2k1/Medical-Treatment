5965359e5ba688b306547c2ef4af234579ca8751
xuanthai2 <68328065+xuanthai2@users.noreply.github.com>
2021-11-17 22:04:00 +0700

    + add register form + gp form

---
+++ MedicalTreament/MedicalTreament/FormRegister.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace MedicalTreament
+{
+    partial class FormRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox_username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.Register_register = new Guna.UI2.WinForms.Guna2Button();
+            this.label_username = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.textbox_password = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label_confirmedpassword = new Guna.UI.WinForms.GunaLabel();
+            this.textbox_confirmedpassword = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label_role = new Guna.UI.WinForms.GunaLabel();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.SuspendLayout();
+            // 
+            // textbox_username
+            // 
+            this.textbox_username.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_username.BorderColor = System.Drawing.Color.Coral;
+            this.textbox_username.BorderRadius = 8;
+            this.textbox_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textbox_username.DefaultText = "";
+            this.textbox_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textbox_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textbox_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_username.DisabledState.Parent = this.textbox_username;
+            this.textbox_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_username.FillColor = System.Drawing.Color.Silver;
+            this.textbox_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_username.FocusedState.Parent = this.textbox_username;
+            this.textbox_username.ForeColor = System.Drawing.Color.Black;
+            this.textbox_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_username.HoverState.Parent = this.textbox_username;
+            this.textbox_username.Location = new System.Drawing.Point(130, 123);
+            this.textbox_username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textbox_username.Name = "textbox_username";
+            this.textbox_username.PasswordChar = '\0';
+            this.textbox_username.PlaceholderForeColor = System.Drawing.Color.White;
+            this.textbox_username.PlaceholderText = "";
+            this.textbox_username.SelectedText = "";
+            this.textbox_username.ShadowDecoration.Parent = this.textbox_username;
+            this.textbox_username.Size = new System.Drawing.Size(267, 44);
+            this.textbox_username.TabIndex = 1;
+            // 
+            // Register_register
+            // 
+            this.Register_register.CheckedState.Parent = this.Register_register;
+            this.Register_register.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Register_register.CustomImages.Parent = this.Register_register;
+            this.Register_register.FillColor = System.Drawing.Color.Coral;
+            this.Register_register.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Register_register.ForeColor = System.Drawing.Color.Black;
+            this.Register_register.HoverState.Parent = this.Register_register;
+            this.Register_register.Location = new System.Drawing.Point(175, 493);
+            this.Register_register.Name = "Register_register";
+            this.Register_register.ShadowDecoration.Parent = this.Register_register;
+            this.Register_register.Size = new System.Drawing.Size(149, 45);
+            this.Register_register.TabIndex = 4;
+            this.Register_register.Text = "Sign Up";
+            // 
+            // label_username
+            // 
+            this.label_username.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_username.AutoSize = true;
+            this.label_username.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label_username.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label_username.Location = new System.Drawing.Point(126, 99);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(75, 20);
+            this.label_username.TabIndex = 2;
+            this.label_username.Text = "Username";
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.Location = new System.Drawing.Point(126, 180);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(70, 20);
+            this.gunaLabel1.TabIndex = 4;
+            this.gunaLabel1.Text = "Password";
+            // 
+            // textbox_password
+            // 
+            this.textbox_password.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_password.BorderColor = System.Drawing.Color.Coral;
+            this.textbox_password.BorderRadius = 8;
+            this.textbox_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textbox_password.DefaultText = "";
+            this.textbox_password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textbox_password.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textbox_password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_password.DisabledState.Parent = this.textbox_password;
+            this.textbox_password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_password.FillColor = System.Drawing.Color.Silver;
+            this.textbox_password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_password.FocusedState.Parent = this.textbox_password;
+            this.textbox_password.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_password.HoverState.Parent = this.textbox_password;
+            this.textbox_password.Location = new System.Drawing.Point(130, 204);
+            this.textbox_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textbox_password.Name = "textbox_password";
+            this.textbox_password.PasswordChar = '\0';
+            this.textbox_password.PlaceholderText = "";
+            this.textbox_password.SelectedText = "";
+            this.textbox_password.ShadowDecoration.Parent = this.textbox_password;
+            this.textbox_password.Size = new System.Drawing.Size(267, 44);
+            this.textbox_password.TabIndex = 2;
+            // 
+            // label_confirmedpassword
+            // 
+            this.label_confirmedpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_confirmedpassword.AutoSize = true;
+            this.label_confirmedpassword.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label_confirmedpassword.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label_confirmedpassword.Location = new System.Drawing.Point(126, 264);
+            this.label_confirmedpassword.Name = "label_confirmedpassword";
+            this.label_confirmedpassword.Size = new System.Drawing.Size(144, 20);
+            this.label_confirmedpassword.TabIndex = 6;
+            this.label_confirmedpassword.Text = "Confirmed Password";
+            // 
+            // textbox_confirmedpassword
+            // 
+            this.textbox_confirmedpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_confirmedpassword.BorderColor = System.Drawing.Color.Coral;
+            this.textbox_confirmedpassword.BorderRadius = 8;
+            this.textbox_confirmedpassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textbox_confirmedpassword.DefaultText = "";
+            this.textbox_confirmedpassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textbox_confirmedpassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textbox_confirmedpassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_confirmedpassword.DisabledState.Parent = this.textbox_confirmedpassword;
+            this.textbox_confirmedpassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textbox_confirmedpassword.FillColor = System.Drawing.Color.Silver;
+            this.textbox_confirmedpassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_confirmedpassword.FocusedState.Parent = this.textbox_confirmedpassword;
+            this.textbox_confirmedpassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textbox_confirmedpassword.HoverState.Parent = this.textbox_confirmedpassword;
+            this.textbox_confirmedpassword.Location = new System.Drawing.Point(130, 288);
+            this.textbox_confirmedpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textbox_confirmedpassword.Name = "textbox_confirmedpassword";
+            this.textbox_confirmedpassword.PasswordChar = '\0';
+            this.textbox_confirmedpassword.PlaceholderText = "";
+            this.textbox_confirmedpassword.SelectedText = "";
+            this.textbox_confirmedpassword.ShadowDecoration.Parent = this.textbox_confirmedpassword;
+            this.textbox_confirmedpassword.Size = new System.Drawing.Size(267, 44);
+            this.textbox_confirmedpassword.TabIndex = 3;
+            // 
+            // label_role
+            // 
+            this.label_role.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_role.AutoSize = true;
+            this.label_role.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label_role.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label_role.Location = new System.Drawing.Point(126, 354);
+            this.label_role.Name = "label_role";
+            this.label_role.Size = new System.Drawing.Size(39, 20);
+            this.label_role.TabIndex = 7;
+            this.label_role.Text = "Role";
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Coral;
+            this.guna2ComboBox1.BorderColor = System.Drawing.Color.Coral;
+            this.guna2ComboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FillColor = System.Drawing.Color.Silver;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.Empty;
+            this.guna2ComboBox1.FocusedState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.Coral;
+            this.guna2ComboBox1.FormattingEnabled = true;
+            this.guna2ComboBox1.HoverState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.Items.AddRange(new object[] {
+            "General Practitional",
+            "Specialist",
+            "Secretary",
+            "Pharmacist"});
+            this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(130, 387);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Size = new System.Drawing.Size(267, 36);
+            this.guna2ComboBox1.TabIndex = 8;
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.Location = new System.Drawing.Point(168, 33);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(191, 41);
+            this.gunaLabel2.TabIndex = 9;
+            this.gunaLabel2.Text = "Registration";
+            // 
+            // FormRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(522, 616);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.label_role);
+            this.Controls.Add(this.label_confirmedpassword);
+            this.Controls.Add(this.textbox_confirmedpassword);
+            this.Controls.Add(this.gunaLabel1);
+            this.Controls.Add(this.textbox_password);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.Register_register);
+            this.Controls.Add(this.textbox_username);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormRegister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox textbox_username;
+        private Guna.UI2.WinForms.Guna2Button Register_register;
+        private Guna.UI.WinForms.GunaLabel label_username;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox textbox_password;
+        private Guna.UI.WinForms.GunaLabel label_confirmedpassword;
+        private Guna.UI2.WinForms.Guna2TextBox textbox_confirmedpassword;
+        private Guna.UI.WinForms.GunaLabel label_role;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+    }
+}
