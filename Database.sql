ef2d6ec0a723c1d0f46fa14e59ab0ff1f0371a7d
xuanthai2 <68328065+xuanthai2@users.noreply.github.com>
2021-12-07 16:46:05 +0700

    +Update Database.sql

--- Database.sql
+++ Database.sql
@@ -165,7 +165,7 @@ insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, Expriratio
 values ('00001','Acetaminophen','effervescent','650 mg','1100','2000','Actamin','1/12/2025','1/12/2021')
 
 insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
-values ('00002','afatinib','oral tablet','30 mg','500','3000','Gilotrif','1/12/2025','1/12/2021')
+values ('00002','Afatinib','oral tablet','30 mg','500','3000','Gilotrif','1/12/2025','1/12/2021')
 
 insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
 values ('00003','Molnupiravir','oral tablet','500 mg','600','8000','Merck','1/12/2025','1/12/2021')
@@ -194,4 +194,33 @@ values ('00010','Ibuprofen','oral capsule','200 mg','830','3000','Advil','1/12/2
 insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
 values ('00011','Cephalexin','oral capsule','250 mg','530','2000','Keflex','1/12/2025','1/12/2021')
 
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00012','Trazodone','oral tablet','100 mg','580','2000','Desyrel','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00013','Methadone','oral tablet','150 mg','1800','400','Dolophine','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00014','Lisinopril','oral tablet','10 mg','770','1500','Prinivil','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00015','Omeprazole','oral delayed release tablet','20 mg','2000','600','FIRST Omeprazole','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00016','Mesalamine','oral delayed release capsule','400 mg','1200','1500','Apriso','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00017','Felodipine','oral tablet','10 mg','1500','700','Plendil','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00018','Flutamide','oral capsule','125 mg','1130','1000','Eulexin','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00019','Flurazepam','oral capsule','15 mg','350','1400','Dalmane','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00020','Osimertinib','oral tablet','40 mg','700','2300','Tagrisso','1/12/2025','1/12/2021')
+
+insert into Drug(DrugID, Name, Type, Unit, Price, Quantity, Producer, ExprirationDate, ImportDate )
+values ('00021','Oxandrolone','oral tablet','10 mg','1700','3100','Oxandrin','1/12/2025','1/12/2021')
 -------------------------------------------------------------------------------------------------------------------
