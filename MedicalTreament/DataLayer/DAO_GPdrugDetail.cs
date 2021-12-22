cb50491e41ca9b091bdc981e8bf3a05f4e805619
Trần Anh Tuấn <19522481@gm.uit.edu.vn>
2021-12-23 00:38:24 +0700

    Merge pull request #37 from AnhTuan2k1/xuanthai2
    Update

---
+++ MedicalTreament/DataLayer/DAO_GPdrugDetail.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DAO_GPdrugDetail
+    {
+        MedicalTreatmentEntities db;
+
+        private static DAO_GPdrugDetail instance;
+        private DAO_GPdrugDetail()
+        {
+            db = new MedicalTreatmentEntities();
+        }
+        public static DAO_GPdrugDetail getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DAO_GPdrugDetail();
+            }
+            return instance;
+        }
+    }
+}
